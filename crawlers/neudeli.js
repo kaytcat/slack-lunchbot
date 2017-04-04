ebb707209e3e1f961e4185794249cd11a10a0e79
Kaitlin Rathwell <kaytcat@gmail.com>
2017-04-04 16:25:52 +0200

    Change to lunch room

--- crawlers/neudeli.js
+++ crawlers/neudeli.js
@@ -2,7 +2,6 @@ var dl = require("../download.js");
 var url = 'http://neudeli.at/wp-content/uploads/2016/10/Wochenkarte-kw42.pdf';
 var converter = require("../pdf-parser.js");
 
-
 module.exports = {
     getMenu: function (postMessage) {
         dl.downloadPDF(url, converter.convert2txt, postMessage);
