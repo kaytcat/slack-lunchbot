cf80450b0354f046bcc7b446b7459018fe4f5be3
Kristof Molnar <kristof.molnar@kununu.com>
2017-04-04 14:46:24 +0200

    Modular design for neudeli+pdfparser

--- pdf-parser.js
+++ pdf-parser.js
@@ -1,21 +1,27 @@
 let fs = require('fs'), PDFParser = require("pdf2json");
-let pdfParser = new PDFParser(this,1);
-try {
-  const args = process.argv[2];
-  console.log(args);
-  var n = args.lastIndexOf('/');
-  var cutname = args.substring(n + 1);
-  var path = "./txt-db/";
+module.exports = {
+  convert2txt: function(file) {
 
-  console.log("Creating txt...");
+    let pdfParser = new PDFParser(this,1);
+    try {
+      var path = "../txt-db/";
+      var pathpdf = "../pdf-db/";
 
-  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-  pdfParser.on("pdfParser_dataReady", pdfData => {
-      fs.writeFile(path+ cutname + ".txt", pdfParser.getRawTextContent());
-  });
+      console.log(pathpdf+ file.filename);
+      console.log("Creating txt...");
+      console.log("////")
+      console.log(path+ JSON.stringify(file.filename).replace(/['"]+/g, '') + ".txt");
+      console.log("////")
 
-  pdfParser.loadPDF(args);
+      pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+      pdfParser.on("pdfParser_dataReady", pdfData => {
+          fs.writeFile(path+ JSON.stringify(file.filename).replace(/['"]+/g, '') + ".txt", pdfParser.getRawTextContent());
+      });
 
-} catch(e) {
-  console.log(e);
+      pdfParser.loadPDF(pathpdf+ file.filename);
+
+    } catch(e) {
+      console.log(e);
+    }
+  }
 }
