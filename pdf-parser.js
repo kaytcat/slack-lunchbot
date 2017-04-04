0f05b8baca8d64cc6d362af5dec8ad79a3d95caa
Kristof Molnar <kristof.molnar@kununu.com>
2017-04-04 12:49:05 +0200

    Exceptions for pdf-parser

--- pdf-parser.js
+++ pdf-parser.js
@@ -1,17 +1,21 @@
 let fs = require('fs'), PDFParser = require("pdf2json");
 let pdfParser = new PDFParser(this,1);
+try {
+  const args = process.argv[2];
+  console.log(args);
+  var n = args.lastIndexOf('/');
+  var cutname = args.substring(n + 1);
+  var path = "./txt-db/";
 
-const args = process.argv[2];
-console.log(args);
-var n = args.lastIndexOf('/');
-var cutname = args.substring(n + 1);
-var path = "./txt-db/";
+  console.log("Creating txt...");
 
-console.log("Creating txt...");
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+      fs.writeFile(path+ cutname + ".txt", pdfParser.getRawTextContent());
+  });
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile(path+ cutname + ".txt", pdfParser.getRawTextContent());
-});
+  pdfParser.loadPDF(args);
 
-pdfParser.loadPDF(args);
+} catch(e) {
+  console.log(e);
+}
