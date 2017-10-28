83ec001cb2552c73277f8dd1c73668241535b1b9
Angela <angela.rumpl@gmail.com>
2017-10-28 23:01:10 +0200

    Add 'Das Heinz'

--- tests/crawlers/dasheinz.js
+++ tests/crawlers/dasheinz.js
@@ -2,8 +2,8 @@
 
 const should = require('should');
 
-const crawler = require('./../../crawlers/aera.js');
-describe('#getMenu() from Aera', function () {
+const crawler = require('./../../crawlers/dasheinz.js');
+describe('#getMenu() from das heinz', function () {
     it('responds with menu', function () {
         return crawler.getMenu().then(function (data) {
             console.log(data);
